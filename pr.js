4340a0c3d27370dd615f6b331289ccd458bb28c3
subhrajeetdas <64862304+subhrajeetdas@users.noreply.github.com>
2020-05-08 11:04:39 +0530

    Create pr.js

---
+++ pr.js
@@ -0,0 +1,109 @@
+ 
+let request;
+ 
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
+    if (!window.PaymentRequest) {
+      return null;
+    }
+    
+    const supportedInstruments = [{
+        supportedMethods: "https://mercury-t2.phonepe.com/transact/pay",
+        data: {
+            url: 'upi://pay?pa=M2306160483220675579140@ybl&pn=HP-TEST&am=10.00&mam=10.00&tid=YBL3c675468ac474731b3139f80dd5d55cd&tr=T2005051604162811320805&tn=Payment+for+quikr_001&mc=5311&mode=04&purpose=00&utm_campaign=DEBIT&utm_medium=M2306160483220675579140&utm_source=ABffeefff2fs'       //redirect url from v4/debit response
+        }
+    }];
+  
+    const details = {
+        id: "1111-71ca4e9f-748c-4de7-af7b-a84f3da75b4e-temp",       //A unique id [optional], if not passed browser will generate one
+      total: {
+        label: 'Total',
+        amount: {
+          currency: 'INR',
+          value: '100',
+        }
+      }
+    };
+  
+    try {
+      request = new PaymentRequest(supportedInstruments, details);
+      console.log("Request: "+request);
+      console.log("request.canMakePayment: "+request.canMakePayment);
+      if (request.canMakePayment) {
+        request.canMakePayment().then(function(result) {
+            //Show “pay by Phonepe” button in payment options
+            //onPayByPhonePeClick();
+            //console.log("Inside Can Make Payment"+result);
+            alert("Hello");
+            alert("Inside Can Make Payment "+result);
+
+        }).catch(function(err) {
+          handleError(err);
+        });
+      }
+      if (request.hasEnrolledInstrument) {
+        request.hasEnrolledInstrument().then(function(result) {
+        }).catch(function(err) {
+          handleError(err);     //handle error
+        });
+      }
+    } catch (e) {
+      handleError(e);
+    }
+  }
+  
+ /**
+  * Create payment request object for Phonepe payment.
+  */
+  function onCheckoutClick(){
+       buildPaymentRequest();
+  }
+ 
+  
+  /**
+   * Handles the response from PaymentRequest.show().
+   */
+  function handlePaymentResponse(response) {
+      //Check if the response.details.result is success
+      //get the transaction ref id from the response
+      //use transaction refId and merchant Id to fetch the status
+      var fetchOptions = {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(payloadForFetch)   
+          };
+          var serverPaymentRequest = new Request('secure/payment/endpoint');    //endpoint to fetch the status from server
+          fetch(serverPaymentRequest, fetchOptions).then( fetchResponse => {
+            if (fetchResponse.status < 400) {
+              response.complete("success");     //notifies the user agent that the user interaction is over, and causes any remaining user interface to be closed
+            } else {
+              response.complete("fail");        //notifies the user agent that the user interaction is over, and causes any remaining user interface to be closed
+            };
+          }).catch( reason => {
+            response.complete("fail");          //notifies the user agent that the user interaction is over, and causes any remaining user interface to be closed
+          });      
+  }
+  
+  /**
+   * Click event listener for “pay by Phonepe” button
+   * Launch payment request for Phonepe payment.
+   */
+  function onPayByPhonePeClick() { 
+    if (!window.PaymentRequest || !request) {
+      return;
+    }
+ 
+      try {
+        request.show()
+          .then(handlePaymentResponse)
+          .catch(function(err) {
+            handleError(err);          //handle error
+            request = buildPaymentRequest();
+          });
+      } catch (e) {
+        handleError(e);
+      }
+  }
