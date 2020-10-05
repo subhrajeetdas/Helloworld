86f0f77659664e22c69022fc0a562e2790f31275
subhrajeetdas <64862304+subhrajeetdas@users.noreply.github.com>
2020-10-05 13:12:29 +0530

    Update pr.js

--- pr.js
+++ pr.js
@@ -17,7 +17,12 @@ function buildPaymentRequest() {
     const supportedInstruments = [{
         supportedMethods: "https://tez.google.com/pay",
         data: {
-            url: ''  //redirect url from v4/debit response
+         pa: 'merchant-vpa@xxx',
+        pn: 'Merchant Name',
+        tr: '1234ABCD',  // your custom transaction reference ID
+        url: 'http://url/of/the/order/in/your/website',
+        mc: '1234', // your merchant category code
+        tn: 'Purchase in Merchant',
         }
     }];
   
